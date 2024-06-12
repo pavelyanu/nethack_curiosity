e2d2248b54847e5c9515356457c669995c78bde2
pavelyanu <pyanushonak@gmail.com>
2024-06-12 11:56:41 +0200

    Set minigrid params to reproduce noveld experiment

--- nethack_curiosity/envs/minigrid/minigrid_env_params.py
+++ nethack_curiosity/envs/minigrid/minigrid_env_params.py
@@ -2,7 +2,20 @@ from argparse import ArgumentParser
 
 
 def minigrid_env_override_defaults(env: str, parser: ArgumentParser):
-    pass
+    parser.set_defaults(
+        num_workers=20,
+        num_envs_per_worker=2,
+        # train_for_env_steps=500000000,
+        train_for_env_steps=1000000,
+        batch_size=32,
+        rollout=100,
+        max_grad_norm=40,
+        exploration_loss="entropy",
+        exploration_loss_coeff=0.001,
+        value_loss_coeff=0.5,
+        gamma=0.99,
+        learning_rate=0.0001,
+    )
 
 
 def add_minigrid_env_args(env: str, parser: ArgumentParser):
